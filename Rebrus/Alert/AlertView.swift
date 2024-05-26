a5356aa60e3cbfd17c20222a50f98ccfd71bfc56
Naz <nazerke.sembay11@mail.ru>
2024-05-26 16:04:56 +0500

    logout alert

---
+++ Rebrus/Alert/AlertView.swift
@@ -0,0 +1,8 @@
+//
+//  AlertView.swift
+//  Rebrus
+//
+//  Created by Nazerke Sembay on 26.05.2024.
+//
+
+import Foundation
---
+++ Rebrus/Alert/AlertViewController.swift
@@ -0,0 +1,8 @@
+//
+//  AlertViewController.swift
+//  Rebrus
+//
+//  Created by Nazerke Sembay on 26.05.2024.
+//
+
+import Foundation
